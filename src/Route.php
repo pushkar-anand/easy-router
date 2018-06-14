6c085c7766ef3922b98625550c9c9b58a198aa9a
Pushkar Anand <anandpushkar088@gmail.com>
2018-06-15 00:50:20 +0530

    Finished writing all methods, logic and flow complete. Testing is not done.

--- src/Route.php
+++ src/Route.php
@@ -1,10 +1,170 @@
 <?php
+/**
+ * Copyright (c) 2018 Pushkar Anand. Under MIT License.Copyright (c) 2018 Pushkar Anand. Under MIT License.
+ *
+ */
 
 namespace EasyRoute;
 
 
+use Exception;
+
+/**
+ * Class Route
+ * @package EasyRoute
+ */
 class Route
 {
+    public $match_found = false;
+    private $ARRAY_METHOD_KEY = "method";
+    private $ARRAY_URI_KEY = "uri";
+    private $ARRAY_CALLABLE_BOOL_KEY = "callable";
+    private $ARRAY_FILE_KEY = "file";
+    private $ARRAY_CALLABLE_KEY = "function";
+    private $server_uri;
+    private $request_method;
+    private $script_name;
+    private $current_path;
+    private $match_list = array();
+    private $match;
+    private $send404 = true;
 
+    /**
+     * Route constructor.
+     */
+    public function __construct()
+    {
+        $this->server_uri = $_SERVER['REQUEST_URI'];
+        $this->request_method = $_SERVER["REQUEST_METHOD"];
+        $this->script_name = $_SERVER['SCRIPT_NAME'];
 
+        $this->current_path = $this->get_request_path($this->server_uri, $this->script_name);
+    }
+
+    /**
+     * @param string $server_uri
+     * @param string $script_name
+     * @return string
+     */
+    private function get_request_path(string $server_uri, string $script_name): string
+    {
+        $basepath = implode('/',
+                array_slice(explode('/', $script_name),
+                    0,
+                    -1)) . '/';
+
+        $uri = substr($server_uri, strlen($basepath));
+
+        if (strstr($uri, '?')) {
+            $uri = substr($uri, 0, strpos($uri, '?'));
+        }
+        $uri = '/' . trim($uri, '/');
+
+        return $uri;
+    }
+
+    /**
+     * If this is set to false 404 NOT FOUND will not be sent when no match is found.
+     * By default set to true.
+     * @param bool $bool
+     */
+    public function send404(bool $bool)
+    {
+        $this->send404 = $bool;
+    }
+
+    /**
+     * Add all routes by calling this method.
+     *
+     * @param string $method is the request method.
+     * @param string $uri is the uri to be matched.
+     * @param string|callable $next is either a file to load or a callable function.
+     * @throws Exception
+     */
+    public function addMatch(string $method, string $uri, $next)
+    {
+        $method = strtoupper($method);
+
+        if ($method != 'POST' || $method != 'GET') {
+            throw new Exception("Method " . $method . " is not supported.");
+        }
+
+        if (!is_string($uri)) {
+            throw new Exception("Uri " . $uri . " is not valid.");
+        }
+
+        if (is_callable($next)) {
+
+            $new_match = array(
+                $this->ARRAY_METHOD_KEY => $method,
+                $this->ARRAY_URI_KEY => $uri,
+                $this->ARRAY_CALLABLE_BOOL_KEY => true,
+                $this->ARRAY_CALLABLE_KEY => $next
+            );
+
+        } elseif (is_string($next)) {
+
+            if (file_exists($next)) {
+                $new_match = array(
+                    $this->ARRAY_METHOD_KEY => $method,
+                    $this->ARRAY_URI_KEY => $uri,
+                    $this->ARRAY_CALLABLE_BOOL_KEY => false,
+                    $this->ARRAY_FILE_KEY => $next
+                );
+
+            } else {
+                $dir_next = __DIR__ . "/" . $next;
+
+                if (file_exists($dir_next)) {
+                    $new_match = array(
+                        $this->ARRAY_METHOD_KEY => $method,
+                        $this->ARRAY_URI_KEY => $uri,
+                        $this->ARRAY_CALLABLE_BOOL_KEY => false,
+                        $this->ARRAY_FILE_KEY => $dir_next
+                    );
+
+                } else {
+
+                    throw new Exception("File " . $next . " not found.");
+                }
+            }
+        } else {
+            throw new Exception("Invalid third parameter. Expecting callable or file.");
+        }
+
+        array_push($this->match_list, $new_match);
+    }
+
+    public function execute()
+    {
+        if ($this->findMatch()) {
+            if ($this->match[$this->ARRAY_CALLABLE_BOOL_KEY] == true) {
+                $callable = $this->match[$this->ARRAY_CALLABLE_KEY];
+                $callable();
+            } else {
+                $file = $this->match[$this->ARRAY_FILE_KEY];
+                require_once $file;
+            }
+        } else {
+            if ($this->send404) {
+                header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+                exit();
+            }
+        }
+    }
+
+    private function findMatch(): bool
+    {
+        foreach ($this->match_list as $match) {
+
+            if ($match[$this->ARRAY_METHOD_KEY] == $this->request_method &&
+                $match[$this->ARRAY_URI_KEY] == $this->current_path) {
+
+                $this->match_found = true;
+                $this->match = $match;
+                return true;
+            }
+        }
+        return false;
+    }
 }
